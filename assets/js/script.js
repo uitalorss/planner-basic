eb4ade9d4013769c47217b84da485eee5718fe84
Uítalo <uitalo.urss@gmail.com>
2022-09-28 22:57:46 -0300

    estilização de itens do planner

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+/** Ações necessárias <header>
+ * Vou precisar fazer uma função para pegar todos os compromissos e salvar no localstorage e atribuir ao botão Salvar localStorage
+ * Vou precisar fazer uma função para apagar os dados do localstorage e atribuir ao botão Excluir localStorage.
+ * Vou precisar pegar os dados de data e hora do computador e linkar ao exemplo de data e hora que está no header.
+ */
+
+/** Ações necessárias <formulário>
+ * Pegar cada item do formulário (atividade, dia da semana, horario (hora:minuto))
+ * Criar um array de objetos para receber esses dados
+ * Criar função para adicionar uma atividade ao array 
+ *    Ao clicar no botão Adicionar atividade, o sistema deve disparar essa função e adicionar no array os dados que estiverem nos inputs
+ *    Tratar campos que estiverem vazios
+ *    Disparar uma mensagem de confirmação ao adicionar uma atividade
+ * Criar função para excluir todas as atividades
+ *  Ao clicar no botão Excluir todos, o sistema deve disparar essa função e excluir todas as tarefas.
+ *  Disparar mensagem de confirmação
+ */
+
+/** Ações necessárias (planner)
+ * Ao clicar em um dia da semana, o sistema deve mostrar as atividades que tem aquele determinado dia atribuído.
+ * A visualização das horas e da atividade tem que estar em cards distintos, conforme wireframe. 
+ * Cada card deverá ter um botão para apagar atividade e atualizar o array das atividades.
+ * Caso haja duas ou mais atividades com o mesmo dia e horário, deve haver uma personalização diferenciada, conforme wireframe.
+*/
